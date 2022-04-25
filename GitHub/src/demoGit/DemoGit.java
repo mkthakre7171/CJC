96fa2b6ab9f2340953981ce8817292a86b8f9c50
ADMIN <ADMIN@CLINT-11>
2022-04-25 12:10:15 +0530

    by mk1

--- GitHub/src/demoGit/DemoGit.java
+++ GitHub/src/demoGit/DemoGit.java
@@ -1,6 +1,10 @@
 package demoGit;
 
 public class DemoGit {
+	
+	public void m1() {
+		System.out.println("M1 method");
+	}
 	public static void main(String[] args) {
 		System.out.println("Demo GitHub");
 	}
